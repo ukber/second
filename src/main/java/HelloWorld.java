8fafad34618fc92c4269346d4625a8a4cd5d3123
silence <silence@666.com>
2019-11-06 20:01:01 +0800

    first

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @author silence
+ * @create 2019-11-06 19:56
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+
+
+        System.out.println("hello world");
+
+    }
+}
